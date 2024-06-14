88e6a553503222de5f50f1187c5727693f8988cc
Alokraj0304 <alokraj0304@gmail.com>
2024-06-15 00:07:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -43,15 +43,36 @@ document.addEventListener('DOMContentLoaded', function() {
         const matches = data.typeMatches[0].seriesMatches[0].seriesAdWrapper.matches;
 
         if (matches && matches.length > 0) {
-            document.getElementById('box13').textContent = `${matches[0].matchInfo.team1.teamName} vs ${matches[0].matchInfo.team2.teamName}:  ${matches[0].matchInfo.status}`;
+            document.getElementById('boxx13').textContent = `${matches[0].matchInfo.team1.teamName} vs ${matches[0].matchInfo.team2.teamName}:  ${matches[0].matchInfo.status}`;
+            document.getElementById('boxx14').textContent = `${matches[1].matchInfo.team1.teamName} vs ${matches[1].matchInfo.team2.teamName}:  ${matches[1].matchInfo.status}`;
+            document.getElementById('boxx15').textContent = `${matches[2].matchInfo.team1.teamName} vs ${matches[2].matchInfo.team2.teamName}: ${matches[2].matchInfo.status}`;
+            document.getElementById('boxx16').textContent = `${matches[3].matchInfo.team1.teamName} vs ${matches[3].matchInfo.team2.teamName}: ${matches[3].matchInfo.status}`;
+            document.getElementById('boxx17').textContent = `${matches[4].matchInfo.team1.teamName} vs ${matches[4].matchInfo.team2.teamName}: ${matches[4].matchInfo.status}`;
+            document.getElementById('boxx18').textContent = `${matches[5].matchInfo.team1.teamName} vs ${matches[5].matchInfo.team2.teamName}: ${matches[5].matchInfo.status}`;
+            document.getElementById('boxx19').textContent = `${matches[6].matchInfo.team1.teamName} vs ${matches[6].matchInfo.team2.teamName}: ${matches[6].matchInfo.status}`;
+            document.getElementById('boxx20').textContent = `${matches[7].matchInfo.team1.teamName} vs ${matches[7].matchInfo.team2.teamName}: ${matches[7].matchInfo.status}`;
+            document.getElementById('boxx21').textContent = `${matches[8].matchInfo.team1.teamName} vs ${matches[8].matchInfo.team2.teamName}:  ${matches[8].matchInfo.status}`;
+            document.getElementById('boxx22').textContent = `${matches[9].matchInfo.team1.teamName} vs ${matches[9].matchInfo.team2.teamName}:  ${matches[9].matchInfo.status}`;
+            document.getElementById('boxx23').textContent = `${matches[10].matchInfo.team1.teamName} vs ${matches[10].matchInfo.team2.teamName}: ${matches[10].matchInfo.status}`;
+            document.getElementById('boxx24').textContent = `${matches[11].matchInfo.team1.teamName} vs ${matches[11].matchInfo.team2.teamName}: ${matches[11].matchInfo.status}`;
+            document.getElementById('boxx25').textContent = `${matches[12].matchInfo.team1.teamName} vs ${matches[12].matchInfo.team2.teamName}: ${matches[12].matchInfo.status}`;
+            document.getElementById('boxx26').textContent = `${matches[13].matchInfo.team1.teamName} vs ${matches[13].matchInfo.team2.teamName}: ${matches[13].matchInfo.status}`;
+            document.getElementById('boxx27').textContent = `${matches[14].matchInfo.team1.teamName} vs ${matches[14].matchInfo.team2.teamName}: ${matches[14].matchInfo.status}`;
+            document.getElementById('boxx28').textContent = `${matches[15].matchInfo.team1.teamName} vs ${matches[15].matchInfo.team2.teamName}: ${matches[15].matchInfo.status}`;
+        } else {
+            console.error('No matches found');
             
-            document.getElementById('box14').textContent = `${matches[1].matchInfo.team1.teamName} vs ${matches[1].matchInfo.team2.teamName}:  ${matches[1].matchInfo.status}`;
-            document.getElementById('box15').textContent = `${matches[2].matchInfo.team1.teamName} vs ${matches[2].matchInfo.team2.teamName}: ${matches[2].matchInfo.status}`;
-            document.getElementById('box16').textContent = `${matches[3].matchInfo.team1.teamName} vs ${matches[3].matchInfo.team2.teamName}: ${matches[3].matchInfo.status}`;
-            document.getElementById('box17').textContent = `${matches[4].matchInfo.team1.teamName} vs ${matches[4].matchInfo.team2.teamName}: ${matches[4].matchInfo.status}`;
-            document.getElementById('box18').textContent = `${matches[5].matchInfo.team1.teamName} vs ${matches[5].matchInfo.team2.teamName}: ${matches[5].matchInfo.status}`;
-            document.getElementById('box19').textContent = `${matches[6].matchInfo.team1.teamName} vs ${matches[6].matchInfo.team2.teamName}: ${matches[6].matchInfo.status}`;
-            document.getElementById('box20').textContent = `${matches[7].matchInfo.team1.teamName} vs ${matches[7].matchInfo.team2.teamName}: ${matches[7].matchInfo.status}`;
+        }
+        if (matches && matches.length > 0) {
+            document.getElementById('one').textContent = `${matches[0].matchInfo.team1.teamSName} vs ${matches[0].matchInfo.team2.teamSName}`;
+            document.getElementById('two').textContent = `${matches[1].matchInfo.team1.teamSName} vs ${matches[1].matchInfo.team2.teamSName}`;
+            document.getElementById('three').textContent = `${matches[2].matchInfo.team1.teamSName} vs ${matches[2].matchInfo.team2.teamSName}`;
+            document.getElementById('four').textContent = `${matches[3].matchInfo.team1.teamSName} vs ${matches[3].matchInfo.team2.teamSName}`;
+            document.getElementById('five').textContent = `${matches[4].matchInfo.team1.teamSName} vs ${matches[4].matchInfo.team2.teamSName}`;
+            document.getElementById('six').textContent = `${matches[5].matchInfo.team1.teamSName} vs ${matches[5].matchInfo.team2.teamSName}`;
+            document.getElementById('seven').textContent = `${matches[6].matchInfo.team1.teamSName} vs ${matches[6].matchInfo.team2.teamSName}`;
+            document.getElementById('eight').textContent = `${matches[7].matchInfo.team1.teamSName} vs ${matches[7].matchInfo.team2.teamSName}`;
+           
         } else {
             console.error('No matches found');
             
@@ -60,3 +81,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
     fetchScores();
 });
+
